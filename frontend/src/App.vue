5938cb2a6b474130788edb57895d1fd071508dac
CrossDev <joancruz0502@hotmail.com>
2024-05-30 15:44:06 -0400

    refactor(frontend): removing imports

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-import {  RouterView } from 'vue-router'
-import DefaultLayout from './layouts/DefaultLayout.vue';
 </script>
-
 <template>
   <div class="w-screen">
-    <DefaultLayout/>
+    <DefaultLayout />
     <RouterView />
   </div>
 </template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,31 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import Calendar from 'primevue/calendar';
-import AutoComplete from 'primevue/autocomplete';
-
-
-const items = [
-    { name: 'Apple' },
-    { name: 'Banana' },
-    { name: 'Orange' }
-];
-
-const search = () => {
-    console.log('search');
-}
-
-const value = ref();
 
 </script>
 
 <template>
     <h1>Hello</h1>
-    <Calendar :pt="{ header: 'focus:yellow-400 p-2' }"
-        v-model="value"
-        showIcon />
-    <AutoComplete v-model="value"
-        :suggestions="items"
-        field="name"
-        dropdown
-        @complete="search" />
 </template>
