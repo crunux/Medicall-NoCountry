6098efd291ae4f280e0b97fd33fc5eb991d2eb3e
CrossDev <joancruz0502@hotmail.com>
2024-05-30 15:28:02 -0400

    feat(frontend):  add login interface

---
+++ frontend/src/modules/auth/components/LoginForm.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+
+const loginForm = reactive({
+    email: '',
+    password: '',
+    accept: false
+});
+
+const login = () => {
+    console.log(loginForm);
+}
+</script>
+<template>
+    <div class="ml-[calc(50% - 187px)] flex items-center justify-center p-4">
+        <div v-focustrap
+            class="w-full card p-4 bg-white rounded-lg shadow-lg border-2 border-gray-1"
+            style="max-width: 375px">
+            <form @submit.prevent="login">
+                <div class="mt-3 p-2 flex flex-col">
+                    <label class=" m-1 text-gray-1"
+                        for="email">Email</label>
+                    <InputText id="email"
+                        v-model="loginForm.email"
+                        type="email"
+                        autofocus />
+                </div>
+
+                <div class="mt-3 p-2 flex flex-col">
+                    <div class="flex justify-between m-1"><label class="text-gray-1"
+                            for="password">Password</label>
+                        <RouterLink class="text-primary"
+                            to="/reset-password">olvidaste tu contrasena?</RouterLink>
+                    </div>
+                    <Password inputId="password"
+                        v-model="loginForm.password"
+                        toggleMask
+                        required
+                        :feedback="false"
+                        inputClass="pass" />
+                </div>
+                <div class="flex flex-col">
+                    <Button type="submit"
+                        label="Ingresar"
+                        class="m-2" />
+                </div>
+            </form>
+
+        </div>
+    </div>
+</template>
+<style scope>
+.pass {
+    width: 100%;
+}
+</style>
--- frontend/src/modules/auth/views/LoginView.vue
+++ frontend/src/modules/auth/views/LoginView.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 <template>
-    <div class="app"></div>
+    <section class="w-[calc(100svw - 1rem)] w-full mt-10 justify-center items-center">
+        <h2 class="text-4xl font-semibold p-2 m-3 text-center">Login</h2>
+        <p class="text-gray-1 text-center mb-6">consulta de manera mas rapida y de forma virtual</p>
+        <LoginForm />
+    </section>
 </template>
 <style scoped></style>
