b61deddd81fb451c2a82297eca7d09154c3d5f73
CrossDev <joancruz0502@hotmail.com>
2024-05-30 21:20:33 -0400

    feat(frontend): add inteface register and form

--- frontend/src/modules/auth/views/RegisterView.vue
+++ frontend/src/modules/auth/views/RegisterView.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts"></script>
 <template>
-    <div class="app"></div>
+    <section class="w-[calc(100svw - 1rem)] w-full justify-center items-center">
+        <h2 class="text-4xl font-semibold p-2 m-3 text-center">Register</h2>
+        <p class="text-gray-1 text-center mb-6">consulta de manera mas rapida y de forma virtual</p>
+        <RegisterForm />
+    </section>
 </template>
