deaf27d7b29fca5031b75b9f7c6dc26eb39980fb
Elia2512 <Eliasmilhernandez195@gmail.com>
2024-06-13 11:42:55 -0400

    refactor(fronend): remove videocall button

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -40,8 +40,6 @@ console.log(menuProfile);
         </div>
         <div class="flex justify-between items-center gap-2 md:px-6 font-medium">
             <div class="flex items-center gap-2">
-                <RouterLink :class="'px-2 bg-gray-0 rounded-md text-xl no-underline'"
-                    to="/videocall">Videocall</RouterLink>
                 <RouterLink
                     :class="!activateRegister ? 'px-2 bg-gray-0 rounded-md text-xl no-underline' : 'text-xl no-underline'"
                     v-if="activateLogin && !userLoggedIn"
