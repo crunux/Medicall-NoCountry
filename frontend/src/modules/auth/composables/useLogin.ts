4d567123f91329e03462e7dad5dca0783144aa65
CrossDev <joancruz0502@hotmail.com>
2024-06-14 18:22:13 -0400

    fix: auth sotre declared

--- frontend/src/modules/auth/composables/useLogin.ts
+++ frontend/src/modules/auth/composables/useLogin.ts
@@ -1,47 +1,47 @@
 import { useRouter } from "vue-router";
-
+import {useAuthStore} from '@/stores/useAuthStore';
 
 const router = useRouter();
 
 let HOST_BACKEND = import.meta.env.VITE_HOST_BACKEND
 const PORT_BACKEND = import.meta.env.VITE_PORT_BACKEND || null
 
-if (PORT_BACKEND){
+if (PORT_BACKEND) {
     HOST_BACKEND = `${HOST_BACKEND}:${PORT_BACKEND}`
 }
 
 const useLogin = () => {
     const store = useAuthStore()
-    const login = async (values) => {
-         try {
-        const response = await fetch(`http://${HOST_BACKEND}/api/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: values.email,
-                password: values.password,
-            }),
-        });
+    const login = async (values: any) => {
+        try {
+            const response = await fetch(`http://${HOST_BACKEND}/api/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: values.email,
+                    password: values.password,
+                }),
+            });
 
-        if (!response.ok) {
-            throw new Error('Error en la solicitud');
+            if (!response.ok) {
+                throw new Error('Error en la solicitud');
+            }
+
+            const data = await response.json();
+            console.log('Respuesta del servidor:', data);
+
+            // Guardar el token en el almacenamiento local
+            store.setToken(data.accesTocke);
+            store.setUser(data.user);
+
+            router.push({ name: 'videocall' });
+
+        } catch (error) {
+            console.error('Error al iniciar sesión:', error);
+            // Aquí puedes manejar el error, como mostrar un mensaje al usuario
         }
-
-        const data = await response.json();
-        console.log('Respuesta del servidor:', data);
-
-        // Guardar el token en el almacenamiento local
-        store.setToken(data.accesTocke);
-        store.setUser(data.user);
-
-        router.push({ name: 'videocall' });
-        
-    } catch (error) {
-        console.error('Error al iniciar sesión:', error);
-        // Aquí puedes manejar el error, como mostrar un mensaje al usuario
-    }
     }
     return {
         login
